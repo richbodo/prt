69d9bcf951a76630869718d0d58655b243b6e1f8
Rich Bodo <richbodo@gmail.com>
2025-09-01 00:06:48 +1200

    feat: implement ContactDetailView with field editing

--- prt_src/tui/widgets/__init__.py
+++ prt_src/tui/widgets/__init__.py
@@ -1,5 +1,13 @@
 """TUI widgets for the PRT application."""
 
 from .base import ConfirmDialog, ModeAwareWidget, StatusBar, ToastNotification
+from .contact_detail import ContactDetailView, FieldEditor
 
-__all__ = ["ModeAwareWidget", "StatusBar", "ToastNotification", "ConfirmDialog"]
+__all__ = [
+    "ModeAwareWidget",
+    "StatusBar",
+    "ToastNotification",
+    "ConfirmDialog",
+    "ContactDetailView",
+    "FieldEditor",
+]
